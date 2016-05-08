7a0eb827b0421e90070c5dcfcfba16bd327c52bd
Xiong Xiong <xiong_xiong@comcast.com>
2016-05-08 14:49:29 -0700

    Question 2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(baz)
+    @baz=baz
+  end
+
+  def bar(arg1, sat: :arg2, dat: arg3)
+    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    # catbazdat
+    "#{arg1}#{@baz}#{sat}"
+  end
 end
